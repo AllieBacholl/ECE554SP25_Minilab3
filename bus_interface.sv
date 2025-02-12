f58818033b2e5453e1a6b1e08a29d160c8cc3f2f
jneau <jneau@ad.engr.wisc.edu>
2025-02-11 18:03:50 -0600

    make inteface for bus_interface.sv

---
+++ bus_interface.sv
@@ -0,0 +1,14 @@
+module bus_interface(
+   input logic          iocs,
+   input logic          iorw,
+   input logic          rda,
+   input logic          tbr,
+   input logic    [7:0] recieve_read_line,
+   output logic         recieve_read_enable,
+   output logic   [7:0] write_line,
+   output logic         transmit_write_enable,
+   output logic         baud_write_enable,
+   inout logic    [7:0] databus,
+);
+
+endmodule
