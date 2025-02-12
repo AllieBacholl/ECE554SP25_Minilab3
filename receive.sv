55148bda87e559b1e5b97b42c760533ffc0f0a79
jneau <jneau@ad.engr.wisc.edu>
2025-02-11 18:10:51 -0600

    make inteface for receive.sv

---
+++ receive.sv
@@ -0,0 +1,11 @@
+module bus_interface(
+   input logic          clk,
+   input logic          rst_n,
+   input logic          rxd,
+   input logic          receive_en,
+   output logic         rda,
+   output logic   [7:0] recieve_read_line,
+
+);
+
+endmodule
